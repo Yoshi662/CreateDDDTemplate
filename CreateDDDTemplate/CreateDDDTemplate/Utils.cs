4f653101dac49216f489c37f8002c1387dfa6983
Jose Ignacio <jose.ibanez@col.vueling.com>
2023-05-08 11:17:44 +0200

    Added utils: GetCurrentPath y ExecuteDotnet

--- CreateDDDTemplate/CreateDDDTemplate/Utils.cs
+++ CreateDDDTemplate/CreateDDDTemplate/Utils.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace CreateDDDTemplate
 {
 	public static class Utils
 	{
+		public static string GetCurrentPath()
+		{
+			return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+		}
+		public static void ExecuteDotNet(string args)
+		{
+			Process dotnet = new Process()
+			{
+				StartInfo = new ProcessStartInfo()
+				{
+					FileName = "dotnet",
+					Arguments = args,
+					CreateNoWindow = true,
+				}
+			};
+			dotnet.Start();
+			dotnet.WaitForExit();
+		}
 	}
 }
