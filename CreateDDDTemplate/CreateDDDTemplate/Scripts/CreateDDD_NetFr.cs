8fada1ba6c42f17165853af06eea9fb65704b51c
Jose Ignacio <jose.ibanez@col.vueling.com>
2023-05-08 11:10:50 +0200

    Create Basic Project Create Basic Project Structure

---
+++ CreateDDDTemplate/CreateDDDTemplate/Scripts/CreateDDD_NetFr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateDDDTemplate.Scripts
+{
+	public class CreateDDD_NetFr : IBuildable
+	{
+		public bool Build()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
